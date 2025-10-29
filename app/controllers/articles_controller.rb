96796f37d7988f832768e884d1deb739ef194984
gformisa <gformisa@redhat.com>
2025-10-29 23:58:20 +0100

    Changes to be committed: 	modified:   app/controllers/articles_controller.rb 	new file:   app/views/articles/show.html.erb 	modified:   config/routes.rb
    Ho aggiunto la route show per l'articolo.
    Ho aggiunto il controller show per l'articolo.
    Ho aggiunto la vista show per l'articolo.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all # This line fetches all articles from the database and assigns them to the @articles instance variable.
   end
+
+  def show # This action is used to show a single article.
+    @article = Article.find(params[:id]) # This line fetches the article from the database and assigns it to the @article instance variable.
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   get "/articles", to: "articles#index" # Define the route for the articles index page.
 
+  # Define the route for the articles show page.  This designates a route parameter. A route parameter captures a segment of the request's path, and puts that value into the params Hash,
+  # which is accessible by the controller action. For example, when handling a request like GET http://localhost:3000/articles/1,
+  # 1 would be captured as the value for :id, which would then be accessible as params[:id] in the show action of ArticlesController.
+  get "/articles/:id", to: "articles#show"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
