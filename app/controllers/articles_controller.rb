040fd113e85e60f7cea8817216f62623f5a2a532
gformisa <gformisa@redhat.com>
2025-10-29 23:25:20 +0100

    Aggiunto il metodo all per il modello Article e la view index.html.erb.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all # This line fetches all articles from the database and assigns them to the @articles instance variable.
   end
 end
