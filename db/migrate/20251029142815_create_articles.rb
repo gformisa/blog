380d586e4acdc79bc36430280d824db44bba0a71
gformisa <gformisa@redhat.com>
2025-10-29 15:51:02 +0100

    Ho aggiunto la migrazione per il modello Article. e un commento per la migrazione.

--- db/migrate/20251029142815_create_articles.rb
+++ db/migrate/20251029142815_create_articles.rb
@@ -1,3 +1,9 @@
+# The call to create_table specifies how the articles table should be constructed. By default, the create_table method adds an id column as an auto-incrementing primary key.
+# So the first record in the table will have an id of 1, the next record will have an id of 2, and so on.
+# Inside the block for create_table, two columns are defined: title and body. These were added by the generator because we included them in our generate command (bin/rails generate model Article title:string body:text).
+# On the last line of the block is a call to t.timestamps. This method defines two additional columns named created_at and updated_at.
+# As we will see, Rails will manage these for us, setting the values when we create or update a model object.
+
 class CreateArticles < ActiveRecord::Migration[8.1]
   def change
     create_table :articles do |t|
