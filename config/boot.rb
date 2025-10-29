c63731330f93c0d1431fd0e33c9dc437eda31318
gformisa <gformisa@redhat.com>
2025-10-29 14:23:06 +0100

    Third commit

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__) # Set the bundle gemfile to the Gemfile in the current directory.
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
