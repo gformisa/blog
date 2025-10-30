21a4c3d37809db46dcf0543eba023dbd60696ec8
gformisa <gformisa@redhat.com>
2025-10-30 01:04:13 +0100

    Sono arrivato a creare un nuovo articolo e a validare i campi. Sezione 7.3.3 del applicazione blog di Rails.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,22 @@ class ArticlesController < ApplicationController
   def show # This action is used to show a single article.
     @article = Article.find(params[:id]) # This line fetches the article from the database and assigns it to the @article instance variable.
   end
+
+  def new # This action is used to create a new article.
+    @article = Article.new # This line creates a new article object and assigns it to the @article instance variable.
+  end
+
+  def create # This action is used to create a new article.
+    @article = Article.new(article_params) # This line creates a new article object and assigns it to the @article instance variable.
+    if @article.save # This line saves the new article to the database.
+      redirect_to article_path(@article) # This line redirects to the show page for the new article.
+    else
+      render :new, status: :unprocessable_entity # This line renders the new template.
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body) # This line allows the title and body parameters to be passed to the create action.
+  end
 end
